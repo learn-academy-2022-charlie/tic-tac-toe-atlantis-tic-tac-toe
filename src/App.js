34fc4d55b20b943f7812a624f5c32fa5bbd87977
Nic Last <niclast7611@gmail.com>
2022-05-31 15:53:08 -0700

    when user one clicks x appears when user two clicks o appears

--- src/App.js
+++ src/App.js
@@ -6,21 +6,40 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: [null, null, null, null, null, null, null, null, null]
+      squares: [null, null, null, null, null, null, null, null, null],
+      player: "❌"
     }
   }
 // THIS COMMENT IS FOR THE EMOJIS ❌ AND ⭕️
+//Pseudo Code for two users
+
+// Code below works as backup   
+// handleGamePlay = (index) => {
+//   let { squares } = this.state
+//   squares[index] = "❌"
+//   this.setState({squares: squares})
+// }
+
 
 handleGamePlay = (index) => {
-  let { squares } = this.state
-  squares[index] = "❌"
+  let { squares, player } = this.state
+  if(player === "❌" ){
+    squares[index] = "❌"
+    this.setState({player: "⭕️"})
+  }else{
+    squares[index] = "⭕️"
+    this.setState({player:"❌"})
+  }
+
   this.setState({squares: squares})
 }
+
 
   render() {
     return(
       <>
         <h1>Tic Tac Toe</h1>
+        <p>{this.state.player}'s turn</p>
         <div className='game-board'>
           {this.state.squares.map((value, index) => {
             return (<Square
